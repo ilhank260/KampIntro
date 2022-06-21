373bec3000fe641dfc457525f9d8b781ee06d7b6
ilhank260 <ilhankoder@gmail.com>
2022-06-22 01:27:50 +0300

    ....

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        //constructor  new'lediğin anda çalışan bloğa denir. newlendiğinde 0 elemanlı items oluşur.
+        public MyList()
+        {
+            items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] tempArray = items; //Geçiçi dizi. geçici dizinin referansı items ın referansı 
+            items = new T[items.Length + 1]; // Eleman sayımı 1 arttırıyorum.
+            for (int i = 0; i < tempArray.Length; i++) //TempArray e tutturduğumuz değerleri items'a ekle.
+            {
+                items[i] = tempArray[i];
+
+            }
+            items[items.Length - 1] = item; // items a gelen son elemanı ekle. 
+        }
+    }
+}
